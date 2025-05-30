ec2211e983bed29a06ee073bcca665e14c5cf697
Morialle Barr <Moriallekbarr@gmail.com>
2025-05-30 12:01:04 +0300

    minor change

--- Assets/scripts/Inventory/MouseFollow.cs
+++ Assets/scripts/Inventory/MouseFollow.cs
@@ -14,7 +14,7 @@ public class MouseFollow : MonoBehaviour
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        Vector2 direction = transform.position - mousePosition;
+        Vector2 direction = mousePosition - mousePosition;
 
         transform.right = -direction;
     }
