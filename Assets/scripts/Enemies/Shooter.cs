9b2bd0880ee2872682db6d30bdcb13e22ff37b4f
Morialle Barr <Moriallekbarr@gmail.com>
2025-06-12 16:10:13 +0300

    Lesson 8.8 + 8.9 (ToolTips)

--- Assets/scripts/Enemies/Shooter.cs
+++ Assets/scripts/Enemies/Shooter.cs
@@ -7,13 +7,28 @@ public class Shooter : MonoBehaviour, IEnemy
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletMoveSpeed;
     [SerializeField] private int burstCount;
-    [SerializeField] private int projectliesPerBurst;
+    [SerializeField] private int projectilesPerBurst;
     [SerializeField][Range(0, 359)] private float angleSpread;
     [SerializeField] private float startingDistance = 0.1f;
     [SerializeField] private float timeBetweenBursts;
     [SerializeField] private float restTime = 1f;
-
+    [SerializeField] private bool stagger;
+    [Tooltip("Stagger has to be enabled for oscillate to work properly.")]
+    [SerializeField] private bool oscillate;
+    
     private bool isShooting = false;
+
+    private void OnValidate() {
+        if (oscillate) { stagger = true; }
+        if (!oscillate) { stagger = false; }
+        if (projectilesPerBurst < 1) { projectilesPerBurst = 1; }
+        if (burstCount < 1) { burstCount = 1; }
+        if (timeBetweenBursts < 0.1f) { timeBetweenBursts = 0.1f; }
+        if (restTime < 0.1f) { restTime = 0.1f; }
+        if (startingDistance < 0.1f) { startingDistance = 0.1f; }
+        if (angleSpread == 0) { projectilesPerBurst = 1; }
+        if (bulletMoveSpeed <= 0) { bulletMoveSpeed = 0.1f; }
+    }
 
     public void Attack()
     {
@@ -27,13 +42,32 @@ public class Shooter : MonoBehaviour, IEnemy
     {
         isShooting = true;
 
-        float startAngle, currentAngle, angleStep;
-
-        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
+        float startAngle, currentAngle, angleStep, endAngle;
+        float timeBetweenProjectiles = 0f;
+        
+        TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+        
+        if (stagger) { timeBetweenProjectiles = timeBetweenBursts / projectilesPerBurst; }
 
         for (int i = 0; i < burstCount; i++)
         {
-            for (int j = 0; j < projectliesPerBurst; j++)
+            if (!oscillate)
+            {
+                TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+            }
+            if (oscillate && i % 2 != 1)
+            {
+                TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep, out endAngle);
+            }
+            else if (oscillate)
+            {
+                currentAngle = endAngle;
+                endAngle = startAngle;
+                startAngle = currentAngle;
+                angleStep *= -1;
+            }
+        
+            for (int j = 0; j < projectilesPerBurst; j++)
             {
                 Vector2 pos = FindBulletSpawnPos(currentAngle);
 
@@ -46,31 +80,31 @@ public class Shooter : MonoBehaviour, IEnemy
                 }
 
                 currentAngle += angleStep;
+
+                if (stagger) { yield return new WaitForSeconds(timeBetweenProjectiles); }
             }
 
             currentAngle = startAngle;
 
-            yield return new WaitForSeconds(timeBetweenBursts);
-            TargetConeOfInfluence(out startAngle, out currentAngle, out angleStep);
-
+            if (!stagger) { yield return new WaitForSeconds(timeBetweenProjectiles); }   
         }
 
         yield return new WaitForSeconds(restTime);
         isShooting = false;
     }
 
-    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep)
+    private void TargetConeOfInfluence(out float startAngle, out float currentAngle, out float angleStep, out float endAngle)
     {
         Vector2 targetDirection = PlayerController.Instance.transform.position - transform.position;
         float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
         startAngle = targetAngle;
-        float endAngle = targetAngle;
+        endAngle = targetAngle;
         currentAngle = targetAngle;
         float halfAngleSpread = 0f;
         angleStep = 0f;
         if (angleSpread != 0)
         {
-            angleStep = angleSpread / (projectliesPerBurst - 1);
+            angleStep = angleSpread / (projectilesPerBurst - 1);
             halfAngleSpread = angleStep / 2f;
             startAngle = targetAngle - halfAngleSpread;
             endAngle = targetAngle + halfAngleSpread;
@@ -88,3 +122,4 @@ public class Shooter : MonoBehaviour, IEnemy
         return pos;
     }
 }
+
